bc7c870c744fb4e3c361a66945c3ec86f49f0955
Camille <camille.bedeche@laposte.net>
2022-10-20 16:03:58 +0200

    add

--- php/traitementQuestionnaire.php
+++ php/traitementQuestionnaire.php
@@ -1,1 +1,23 @@
 <?php
+/*se connecter à la base donnée*/
+require_once 'config.php';
+
+
+/*vérifier si les variables $_POST existe grace à isset([nom_du_post])*/
+if(isset($_POST['nomPrenom']) && isset($_POST['mail']) && isset($_POST['telephone']) && isset($_POST['votreProjet']))
+{
+  /*empecher les utilisateurs d'envoyer du codes nuisibles*/
+  $nomPrenom = htmlspecialchars($_POST['nomPrenom']);
+  $mail = htmlspecialchars($_POST['mail']);
+  $telephone = htmlspecialchars($_POST['telephone']);
+  $votreProjet = htmlspecialchars($_POST['votreProjet']);
+
+  $insert = $bdd->prepare('INSERT INTO questionnaire(nomPrenom, mail, telephone, votreProjet) VALUES(:nomPrenom, :mail, :telephone, :votreProjet )');
+  $insert->execute(array(
+    'nomPrenom' => $nomPrenom,
+    'mail' => $mail,
+    'telephone' => $telephone,
+    'votreProjet' => $votreProjet
+  ));
+}
+
